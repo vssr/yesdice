c58ec9222f640c1c8ba4ddf5f377f9bd35c28378
rieks <rieksje@gmail.com>
2015-04-17 20:20:31 +0200

    Added dice.roll method and basic randomization method

--- scripts/yesdice.js
+++ scripts/yesdice.js
@@ -4,34 +4,50 @@
  */
 
 
-/** yesDice initiation object */
+/** yesDice lib initiation object */
 function yesDice(){
 
     // current games
     var diceGames = undefined;
 
 
+    /** main diceGame object, represents one session of dice throwing*/
+    function diceGame(){
+
+    }
+
+    /** dicePool object, represents dice on the 'table' in this diceGame */
+    function dicePool(){
+
+    }
+
+    /** dice object, represents a single die */
+    function dice(){
+
+
+        function roll(){
+            var result = getRandomInt(1,6);
+            consoleLog(result);
+        }
+
+
+    }
+
+    /** diceSide object, represents a side or facing of a die */
+    function diceSide() {
+
+    }
 
 }
 
-/** main diceGame object, represents one session of dice throwing*/
-function diceGame(){
+/** @section randomization functions */
 
-}
-
-/** dicePool object, represents dice on the 'table' in this diceGame */
-function dicePool(){
-
-}
-
-/** dice object, represents a single die */
-function dice(){
-
-}
-
-/** diceSide object, represents a side or facing of a die */
-function diceSide() {
-
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
